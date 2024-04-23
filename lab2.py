58a5168d4c2e2ebf9efa9cdf99d2c64cce954caa
erika <erikakohyl.23@ichat.sp.edu.sg>
2024-04-23 14:19:31 +0800

    bmi example

--- lab2.py
+++ lab2.py
@@ -1,1 +1,22 @@
 print("ET0735(DevOps for AIoT) - Lab 2 - Introduction to Python")
+def funcName(parameter1, parameter2):
+    print("this is a dummy function")
+    return 10
+
+def calculate_bmi(weight,height):
+    print("Height="+str(height))
+    print("Weight="+str(weight))
+    bmi = weight/(height*height)
+    if bmi>25.0:
+        print(str(bmi))
+        print("User is over weight")
+    elif bmi <18.5:
+        print(str(bmi))
+        print("User is under weight")
+    else:
+        print(str(bmi))
+        print("User has a normal weight")
+
+    
+
+calculate_bmi(weight=57,height=1.73)
